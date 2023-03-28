4ae265458073007e3f930539527ec90245ff5d1a
Matej Fabijanic <matej.fabijanic@fer.hr>
2023-03-28 11:58:18 +0200

    first commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+import MovieAppData
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let details = MovieUseCase().getDetails(id: 111161)
+        print(details)
+    }
+}
